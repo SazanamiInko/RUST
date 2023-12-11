f8f61ac78d6cd388e29d3eea8bfcb2a43e38fe4c
SazanamiInko <s.suzuki1614@gmail.com>
2023-12-11 18:16:09 +0900

    所有権エラー(ビルドできません)

---
+++ PG0004_MutX/target/debug/deps/PG0004_MutX-aa8f962b5caf16f6.d
@@ -0,0 +1,5 @@
+C:\Users\Public\Documents\Rust\PG0004_MutX\target\debug\deps\PG0004_MutX-aa8f962b5caf16f6.rmeta: src\main.rs
+
+C:\Users\Public\Documents\Rust\PG0004_MutX\target\debug\deps\PG0004_MutX-aa8f962b5caf16f6.d: src\main.rs
+
+src\main.rs:
---
+++ PG0004_MutX/target/debug/deps/PG0004_MutX-b030d904f065042e.d
@@ -0,0 +1,5 @@
+C:\Users\Public\Documents\Rust\PG0004_MutX\target\debug\deps\PG0004_MutX-b030d904f065042e.rmeta: src\main.rs
+
+C:\Users\Public\Documents\Rust\PG0004_MutX\target\debug\deps\PG0004_MutX-b030d904f065042e.d: src\main.rs
+
+src\main.rs:
